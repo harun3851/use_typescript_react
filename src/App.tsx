f83c9c8683882e468af921bce54e1d670820078b
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-11 23:46:54 +0300

    basic exaple about typescript for heading and section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import PostList from "./components/postList/PostList";
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
 
 type Props = {};
 
 const App = () => {
   return <div>
-    <PostList />
+    <Heading title={"hello"} />
+    <Section > this is my section </Section>
   </div>;
 };
 
